0571bbc51c6f0ff5e401bab09c0b728543cb4602
Ritesh Kumar <rav332211@gmail.com>
2025-05-15 07:33:28 +0530

    changes

--- app/about/company/page.js
+++ app/about/company/page.js
@@ -1,7 +1,22 @@
-export default function company(){
-    return (
-        <div>
-        <h1 className="bg-white text-black">this is my company </h1>
-        </div>
-    )
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+
+export default function Company() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const user = searchParams.get('user');
+
+  return (
+    <div>
+      <h1 className="bg-white text-black">This is my company for {user}</h1>
+      <button
+        className="bg-black text-white px-4 py-2 mt-4"
+        onClick={() => router.push('/page/ritesh')}
+      >
+        Go to Ritesh's Page
+      </button>
+    </div>
+  );
 }
